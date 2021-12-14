97ac58c296dce7c5e6842c196b5ebb282d430021
vi_ctor <victor.fernandes@dextra-sw.com>
2021-12-14 10:24:10 -0300

    fix: heroku deploy

--- bin/main.js
+++ bin/main.js
@@ -3,6 +3,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const Bot_1 = __importDefault(require("./client/Bot"));
 require("dotenv/config");
+const Bot_1 = __importDefault(require("./client/Bot"));
 new Bot_1.default().start();
